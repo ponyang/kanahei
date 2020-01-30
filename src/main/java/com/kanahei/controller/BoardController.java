264e64d32e4d146b07fc0c2e36740a377925dd39
고혜영 <popo1211@tmon.co.kr>
2020-01-31 03:46:41 +0900

    index.html 노출을 위한 설정 시도.. 여전히 안됨 ㅎㅎ

--- src/main/java/com/kanahei/controller/BoardController.java
+++ src/main/java/com/kanahei/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.kanahei.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 }
--- src/main/java/com/kanahei/controller/WebController.java
+++ src/main/java/com/kanahei/controller/WebController.java
@@ -1,13 +1,23 @@
 package com.kanahei.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+@Slf4j
 @Controller
+@RequestMapping("/")
 public class WebController {
-    @RequestMapping(value={"", "/", "/*"}, method = RequestMethod.GET)
+
+    @Value("${test.name}")
+    private String test;
+
+    @GetMapping("/index")
     public String index(){
+        log.error(test);
+        log.error("------ web controller----");
         return "index.html";
     }
 }
