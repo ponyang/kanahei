704be36fa87c91d0efab44ce83673af153d6b03f
고혜영 <popo1211@tmon.co.kr>
2020-02-18 01:35:07 +0900

    router + image load test

--- kanahei-webpage/src/router/index.js
+++ kanahei-webpage/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ImageManage from '@/components/ImageManage'
+import Index from '@/components/Index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/image',
+      name: '이미지 관리',
+      component: ImageManage
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Index
     }
   ]
 })
